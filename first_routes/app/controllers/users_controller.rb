9edc7a9b53359f73c621a8ed06dc317194dc2e13
JortzFromSchool <johnnydortiz@gmail.com>
2021-09-15 15:34:39 -0400

    controller methods complete

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -1,13 +1,45 @@
 class UsersController < ApplicationController
     def index 
-        render plain: "I'm in the index action"
+        render json: User.all
     end
 
     def create
-        render json: params
+        user = User.new(user_params)
+        # replace the `user_attributes_here` with the actual attribute keys
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
-    def show 
-        render json: params
+    def show
+
+        render json: User.find(params[:id])
     end
+
+    def update
+        user = User.find(params[:id])
+        if user.update(user_params)
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        index
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
+
+    # def update_params
+    #     params.require(:user)
+    # end
 end
